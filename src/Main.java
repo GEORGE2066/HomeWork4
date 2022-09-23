95ea79559ec0b9e364eecfe20ec24be07856d693
Администратор <george2066@yandex.ru>
2022-09-23 23:15:18 +0300

    Четвёртое домашнее задание

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.sql.SQLOutput;
+import java.time.Year;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -55,5 +58,89 @@ public class Main {
 
         /*ДОМАШНЕЕ ЗАДАНИЕ №2*/
         /*ПЕРВОЕ ЗАДАНИЕ*/
+
+        int vasiliyDepoz = 15000;
+        i = 0;
+
+        while (vasiliyDepoz <= 12000000) {
+            vasiliyDepoz = (int) (vasiliyDepoz + (vasiliyDepoz * 0.07));
+            i++;
+        }
+        System.out.println(i);//наверное так
+        System.out.println();
+
+
+        /*ВТОРОЕ ЗАДАНИЕ*/
+
+        vasiliyDepoz = 15000;
+        i = 0;
+
+        while (vasiliyDepoz < 12_000_000) {
+            vasiliyDepoz = (int) (vasiliyDepoz + (vasiliyDepoz * 0.07));
+            i++;
+            if (i % 6 == 0) {
+                System.out.println(i + " " + vasiliyDepoz);
+            }
+        }
+        System.out.println();
+
+
+        /*ТРЕТЬЕ ЗАДАНИЕ*/
+
+        long vd = 15_000l;
+        i = 0;
+        j = 0;
+
+        while (i <= (9 * 12)) {
+            vd = (long) (vd + (vd * 0.07));
+            i++;
+            if (i % 6 == 0) {
+                System.out.println(i + " " + vd);
+            }
+        }
+        System.out.println();
+
+
+
+        /*ЧЕТВЁРТОЕ ЗАДАНИЕ*/
+
+        int firstFryday = 4;
+        i = 0;
+
+        while (i <= 31) {
+            if ((i - firstFryday) % 7 == 0) {
+                System.out.println("Сегодня пятница, " + i + "-е число. Необходимо подготовить отчет.");
+            }
+            i++;
+        }
+        System.out.println();
+
+
+
+        /*ДОМАШНЕЕ ЗАДАНИЕ №3*/
+        /*ПЕРВОЕ ЗАДАНИЕ*/
+
+        int year = 2022;
+        i = year - 200;
+        int a = i + 79;
+        while (a % 79 != 0) {
+            a = a - 1;
+        }
+        while (a < 2022) {
+            a += 79;
+            System.out.println(a);
+        }
+        System.out.println(a);
+        System.out.println();
+
+
+        /*ВТОРОЕ ЗАДАНИЕ*/
+
+        i = 1;
+
+        while (i <= 10) {
+            System.out.println("2 * " + i + " = " + (2 * i));
+            i++;
+        }
     }
 }
